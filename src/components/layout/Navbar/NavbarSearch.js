8d4ba60d3e8951952e5fb38fe585e2cbe75edd4a
Zumi <userzumi@gmail.com>
2021-01-01 11:42:22 +0700

    update: remove unused packages, add search handler

--- src/components/layout/Navbar/NavbarSearch.js
+++ src/components/layout/Navbar/NavbarSearch.js
@@ -1,3 +1,4 @@
+import { useFormik } from 'formik'
 import React, { memo } from 'react'
 import {
   Form,
@@ -7,20 +8,35 @@ import {
   InputGroupText
 } from 'shards-react'
 
-const NavbarSearch = memo(() => (
-  <Form className="main-navbar__search w-100 d-none d-md-flex d-lg-flex">
-    <InputGroup seamless className="ml-3">
-      <InputGroupAddon type="prepend">
-        <InputGroupText>
-          <i className="material-icons">search</i>
-        </InputGroupText>
-      </InputGroupAddon>
-      <FormInput
-        className="navbar-search"
-        placeholder="Search for something..."
-      />
-    </InputGroup>
-  </Form>
-))
+const NavbarSearch = memo(() => {
+  const formik = useFormik({
+    initialValues: { q: '' },
+    onSubmit({ q }) {
+      console.log(`You searched "${q}"`)
+    }
+  })
+
+  return (
+    <Form
+      className="main-navbar__search w-100 d-none d-md-flex d-lg-flex"
+      onSubmit={formik.handleSubmit}
+    >
+      <InputGroup seamless className="ml-3">
+        <InputGroupAddon type="prepend">
+          <InputGroupText>
+            <i className="material-icons">search</i>
+          </InputGroupText>
+        </InputGroupAddon>
+        <FormInput
+          id="q"
+          name="q"
+          className="navbar-search"
+          placeholder="Search for something..."
+          {...formik.getFieldProps('q')}
+        />
+      </InputGroup>
+    </Form>
+  )
+})
 
 export default NavbarSearch
