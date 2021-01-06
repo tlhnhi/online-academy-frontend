4dcc791d59d3449b510af6c8c6a4b5d07397dbbc
nhphat22 <1753083.17clc@gmail.com>
2021-01-07 02:13:48 +0700

    update sidebar + enrolled course view

--- src/components/layout/Navbar/NavbarNav/NavbarNav.js
+++ src/components/layout/Navbar/NavbarNav/NavbarNav.js
@@ -1,27 +1,11 @@
 import React, { memo, useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { Nav } from 'shards-react'
-<<<<<<< HEAD
-// import BecomeLecturer from './BecomeLecturer'
-// import Notifications from './Notifications'
-=======
 import BecomeLecturer from './BecomeLecturer'
 import GroupButton from './GroupButton'
 import Notifications from './Notifications'
->>>>>>> d36c07779e0621de49990eca0ecc28df413e1583
 import UserActions from './UserActions'
 
-<<<<<<< HEAD
-const NavbarNav = memo(() => (
-  <Nav navbar className="border-left flex-row">
-    {/* <Notifications /> */}
-    {/* <BecomeLecturer /> */}
-    <UserActions />
-    {/* <GroupButton /> */}
-
-  </Nav>
-))
-=======
 const NavbarNav = memo(() => {
   const authToken = useSelector(state => state.auth)
 
@@ -51,6 +35,5 @@ const NavbarNav = memo(() => {
     </Nav>
   )
 })
->>>>>>> d36c07779e0621de49990eca0ecc28df413e1583
 
 export default NavbarNav
