c0cb9e981c643ea9cef9a04d619b8f58e563dacf
Zumi <userzumi@gmail.com>
2021-01-07 11:53:22 +0700

    update navbar user actions w/ auth check

--- src/components/layout/Navbar/NavbarNav/UserActions.js
+++ src/components/layout/Navbar/NavbarNav/UserActions.js
@@ -1,5 +1,5 @@
-import React, { memo, useCallback, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { memo, useCallback, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import {
   Collapse,
@@ -11,16 +11,50 @@ import {
   NavLink
 } from 'shards-react'
 import { clearAuthToken } from 'store/app/auth'
+import { removeCurrentUser } from 'store/app/current-user'
 
 const UserActions = memo(() => {
+  const currentUser = useSelector(state => state.currentUser)
   const [visible, setVisible] = useState(false)
 
   const dispatch = useDispatch()
 
   const handleSignOut = useCallback(() => {
     dispatch(clearAuthToken())
+    dispatch(removeCurrentUser())
     localStorage.removeItem('token')
   }, [dispatch])
+
+  const student = useMemo(
+    () => (
+      <>
+        <Link to="/my-courses" style={{ textDecoration: 'none' }}>
+          <DropdownItem to="my-course">
+            <i className="material-icons">&#xE2C7;</i> My Courses
+          </DropdownItem>
+        </Link>
+        <Link to="/watchlist" style={{ textDecoration: 'none' }}>
+          <DropdownItem to="watchlist">
+            <i className="material-icons">&#xE896;</i> Watchlist
+          </DropdownItem>
+        </Link>
+      </>
+    ),
+    []
+  )
+
+  const lecturer = useMemo(
+    () => (
+      <>
+        <Link to="/create-course" style={{ textDecoration: 'none' }}>
+          <DropdownItem to="create-course">
+            <i className="material-icons">&#xe145;</i> Create New Course
+          </DropdownItem>
+        </Link>
+      </>
+    ),
+    []
+  )
 
   return (
     <NavItem tag={Dropdown} caret toggle={() => setVisible(!visible)}>
@@ -46,21 +80,7 @@ const UserActions = memo(() => {
             <i className="material-icons">&#xE8B8;</i> Change Password
           </DropdownItem>
         </Link>
-        <Link to="/my-courses" style={{ textDecoration: 'none' }}>
-          <DropdownItem to="my-course">
-            <i className="material-icons">&#xE2C7;</i> My Courses
-          </DropdownItem>
-        </Link>
-        <Link to="/watchlist" style={{ textDecoration: 'none' }}>
-          <DropdownItem to="watchlist">
-            <i className="material-icons">&#xE896;</i> Watchlist
-          </DropdownItem>
-        </Link>
-        <Link to="/create-course" style={{ textDecoration: 'none' }}>
-          <DropdownItem to="create-course">
-            <i className="material-icons">&#xe145;</i> Create New Course
-          </DropdownItem>
-        </Link>
+        {currentUser?.isLecturer ? lecturer : student}
         <DropdownItem divider />
         <DropdownItem className="text-danger" onClick={handleSignOut}>
           <i className="material-icons text-danger">&#xE879;</i> Logout
